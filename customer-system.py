f9ff618f151da7253231115f85dd39e0f6612884
yippii <liboris20@gmail.com>
2026-05-15 15:08:16 -0400

    Added color change for entry box on error, and custom file name support

--- customer-system.py
+++ customer-system.py
@@ -38,11 +38,6 @@ class GUI(tk.Tk):
         self.bind("<Escape>", self._on_escape)
 
     def _build_ui(self):
-        # Segoe UI Font
-        default_font = font.nametofont("TkDefaultFont")
-        default_font.configure(family="Segoe UI Variable",
-                               size=15, weight="normal")
-
         # Tab creation
         self.notebook1 = ttk.Notebook(self)
         self.notebook1.place(x=0, y=0, width=700, height=600)
@@ -56,41 +51,41 @@ class GUI(tk.Tk):
         # Entry creations
         self.firstNameLabel = tk.Label(
             self._tab_notebook1_0, text="First Name")
-        self.firstNameLabel.place(x=56, y=42)
+        self.firstNameLabel.place(x=56, y=45)
 
         self.firstName = ttk.Entry(self._tab_notebook1_0)
         self.firstName.place(x=176, y=32, width=448, height=50)
 
         self.lastNameLabel = tk.Label(self._tab_notebook1_0, text="Last Name")
-        self.lastNameLabel.place(x=56, y=112)
+        self.lastNameLabel.place(x=56, y=115)
 
         self.lastName = ttk.Entry(self._tab_notebook1_0)
         self.lastName.place(x=176, y=102, width=448, height=50)
 
         self.cityLabel = tk.Label(self._tab_notebook1_0, text="City")
-        self.cityLabel.place(x=56, y=182)
+        self.cityLabel.place(x=56, y=185)
 
         self.city = ttk.Entry(self._tab_notebook1_0)
         self.city.place(x=176, y=172, width=448, height=50)
 
         self.postalCodeLabel = tk.Label(
             self._tab_notebook1_0, text="Postal Code")
-        self.postalCodeLabel.place(x=56, y=252)
+        self.postalCodeLabel.place(x=56, y=255)
 
         self.postalCode = ttk.Entry(self._tab_notebook1_0)
         self.postalCode.place(x=176, y=242, width=448, height=50)
 
         self.creditCardNumberLabel = tk.Label(
             self._tab_notebook1_0, text="Card Number")
-        self.creditCardNumberLabel.place(x=56, y=322)
+        self.creditCardNumberLabel.place(x=56, y=325)
 
         self.creditCardNumber = ttk.Entry(self._tab_notebook1_0)
         self.creditCardNumber.place(x=176, y=312, width=448, height=50)
 
         self.birthDateLabel = tk.Label(
-            self._tab_notebook1_0, text="Birth Date\n (YYYY-MM-DD)"
+            self._tab_notebook1_0, text="Birth Date\n(YYYY-MM-DD)", justify="left"
         )
-        self.birthDateLabel.place(x=40, y=380)
+        self.birthDateLabel.place(x=56, y=385)
 
         self.birthDate = ttk.Entry(self._tab_notebook1_0)
         self.birthDate.place(x=176, y=382, width=448, height=50)
@@ -106,6 +101,12 @@ class GUI(tk.Tk):
         )
         self.submitButton.place(x=176, y=452, width=448, height=30)
 
+        self.customerInfoLocationLabel = ttk.Label(self._tab_notebook1_1, text="Path to save")
+        self.customerInfoLocationLabel.place(x=125, y=32, width=448, height=50)
+
+        self.customerInfoLocation = ttk.Entry(self._tab_notebook1_1)
+        self.customerInfoLocation.place(x=125, y=72, width=448, height=50)
+
         self.generateCustomerInfoButton = ttk.Button(
             self._tab_notebook1_1,
             text="Generate Customer File",
@@ -114,14 +115,14 @@ class GUI(tk.Tk):
             style="Accent.TButton",
         )
         self.generateCustomerInfoButton.place(
-            x=125, y=100, width=448, height=100)
+            x=125, y=140, width=448, height=100)
 
         self.customerInfoLocationTipLabel = ttk.Label(
             self._tab_notebook1_1,
-            text="Customer Info Location:\n ~/Documents/customer_info.csv",
+            text="Customer Info Location:\n ~/Documents",
         )
         self.customerInfoLocationTipLabel.place(
-            x=125, y=200, width=448, height=50)
+            x=125, y=240, width=448, height=50)
 
     # Methods
 
@@ -148,6 +149,36 @@ class GUI(tk.Tk):
             )
         except backend.GUIError as e:
             messagebox.showerror(title="Error", message=f"{e}")
+            if str(e) == "Please enter your first name":
+                self.firstName.state(["invalid"])
+            else:
+                self.firstName.state(["!invalid"])
+
+            if str(e) == "Please enter your last name":
+                self.lastName.state(["invalid"])
+            else:
+                self.lastName.state(["!invalid"])
+
+            if str(e) == "Please enter your city":
+                self.city.state(["invalid"])
+            else:
+                self.city.state(["!invalid"])
+
+            if str(e) == "Please enter a valid postal code":
+                self.postalCode.state(["invalid"])
+            else:
+                self.postalCode.state(["!invalid"])
+
+            if str(e) == "Please enter a valid credit card number":
+                self.creditCardNumber.state(["invalid"])
+            else:
+                self.creditCardNumber.state(["!invalid"])
+
+            if "birth date" in str(e):
+                self.birthDate.state(["invalid"])
+            else:
+                self.birthDate.state(["!invalid"])
+
         else:
             self.firstName.delete(0, "end")
             self.lastName.delete(0, "end")
@@ -159,13 +190,20 @@ class GUI(tk.Tk):
     # Button handler for the generate data file button
     def _generate_button_action(self):
         try:
+            if self.customerInfoLocation.get().strip() == "" or self.customerInfoLocation.get().strip()[-4:] != ".csv":
+                print(self.customerInfoLocation.get().strip()[-4:])
+                raise backend.GUIError("Please enter a valid customer info path")
             backend.generateCustomerDataFile(
                 backend.customerInfo,
-                os.path.expanduser("~/Documents/customer_info.csv"),
+                os.path.expanduser("~/Documents/"+self.customerInfoLocation.get()),
             )
         except backend.GUIError as e:
             messagebox.showerror(title="Error", message=f"{e}")
-
+            if str(e) == "Please enter a valid customer info path":
+                self.customerInfoLocation.state(["invalid"])
+        else:
+            self.customerInfoLocation.state(["!invalid"])
+            self.customerInfoLocation.delete(0, "end")
 
 if __name__ == "__main__":
     app = GUI()
